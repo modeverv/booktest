44182786e692bccdc8b04eb8518832e675140f3d
modeverv <modeverv@gmail.com>
2018-07-01 23:35:25 +0900

    2018-07-01 23:35:25

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 # bundle exec asciidoctor --help
 bundle exec asciidoctor -r asciidoctor-diagram src/index.adoc -D docs
-bundle exec asciidoctor-pdf -r asciidoctor-diagram src/index.adoc -o docs/output.pdf
+bundle exec asciidoctor-pdf -r asciidoctor-diagram src/index.adoc -o src/output.pdf
+cp -R src/images/* docs/images
+mv src/output.pdf docs/output.pdf
